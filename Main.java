7e85755ec8b0b5cc1b7902b96bac066c226d66e0
Kenny <yu.kenny.k@gmail.com>
2021-03-16 04:17:04 +0000

    cleanup imports

--- Main.java
+++ Main.java
@@ -1,5 +1,6 @@
 class Main {
   public static void main(String[] args) {
+      // https://www.gitpod.io/docs/languages/java/
     System.out.println("Hello world!");
     String msg = "I just got started";
     System.out.println(msg);   
