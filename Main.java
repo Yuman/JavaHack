072393d2701643733b8d5a980c542ff85d796e7d
Kenny <yu.kenny.k@gmail.com>
2021-03-17 03:24:34 +0000

    added arraylistiteration

--- Main.java
+++ Main.java
@@ -1,6 +1,25 @@
-class Main {
-  public static void main(String[] args) {
-    System.out.println("Hello world!");
-    Day5of30.main(new String[0]);
-  }
+import java.util.ArrayList;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        // Creating the ArrayList
+        ArrayList<Float> fishLengthList = new ArrayList<Float>();
+
+        // Adding the floats
+        fishLengthList.add(10.0f);
+        fishLengthList.add(15.5f);
+        fishLengthList.add(18.0f);
+        fishLengthList.add(29.5f);
+        fishLengthList.add(45.5f);
+
+        // Printing out only the numbers larger than the required length
+        final float requiredLength = 28f;
+        for (Float length : fishLengthList) {
+            if (length >= requiredLength) {
+                System.out.println(length);
+            }
+        }
+    }
 }
