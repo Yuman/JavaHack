42d0827fc4cc64ef3103207cadeb34d6f54de1dc
Kenny <yu.kenny.k@gmail.com>
2021-03-28 04:01:46 +0000

    debugging power of 2

---
+++ challenge/PowerOfTwo.java
@@ -0,0 +1,15 @@
+package challenge;
+public class PowerOfTwo{
+	public static boolean powerOfTwo(int N){
+		if(N == 0){
+			return false;
+		}
+
+		for(int i = 0; i <= Math.sqrt(N); i++){
+			if(Math.pow(2, i) == N){
+				return true;
+			} 
+		}
+		return false;
+	}
+}
